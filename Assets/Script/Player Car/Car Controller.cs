d70b2b93ac1ce0469f29622b8f30bc7f3569a986
Gokul Surendran <105282583+am-Limitless@users.noreply.github.com>
2024-09-04 15:25:14 +0530

    small changes to driving system

--- Assets/Script/Player Car/Car Controller.cs
+++ Assets/Script/Player Car/Car Controller.cs
@@ -2,90 +2,110 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private float horizontalInput;
-    private float verticalInput;
-    private float currentSteerAngle;
-    private float currentBrakeForce;
-    private bool isBraking = false;
+    // Inputs
+    private float _horizontalInput;
+    private float _verticalInput;
+    private bool _isBraking;
 
-    [SerializeField] float engineForce;
-    [SerializeField] float brakeForce;
-    [SerializeField] float maxSteerAngle;
-    [SerializeField] float maxSpeed;
+    // Car controls
+    private float _currentSteerAngle;
+    private float _currentBrakeForce;
 
+    // Seriaalized fields for easy tuning in the Unity Editor
+    [SerializeField] private float engineForce;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private float maxSteerAngle;
+    [SerializeField] private float maxSpeed;
+
+    // Wheel colliders for handling car physics
     [SerializeField] WheelCollider frontLeftWheelCollider;
     [SerializeField] WheelCollider frontRightWheelCollider;
     [SerializeField] WheelCollider rearLeftWheelCollider;
     [SerializeField] WheelCollider rearRightWheelCollider;
 
-
+    // Wheel transforms for updating visual position and rotation
     [SerializeField] Transform frontLeftWheelTransform;
     [SerializeField] Transform frontRightWheelTransform;
     [SerializeField] Transform rearLeftWheelTransform;
     [SerializeField] Transform rearRightWheelTransform;
 
+    private Rigidbody _rigidbody;
+
     private void Start()
     {
-        Rigidbody body = GetComponent<Rigidbody>();
-
-        body.centerOfMass = new Vector3(0, -0.5f, 0);
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = new Vector3(0, -0.5f, 0);
     }
 
     private void FixedUpdate()
     {
-        Speed();
         GetInput();
-        HandleEngine();
+        LimitSpeed();
+        HandleMotor();
         HandleSteering();
-        ApplyBraking();
-        MoveWheels();
+        HandleBraking();
+        UpdateWheels();
         LimitSpeedDuringBraking();
     }
 
-    private void Speed()
+    // Get user input for steering, acceleration, and braking
+    private void GetInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        _isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    // Limit the car's speed to the maximum speed set in the editor
+    private void LimitSpeed()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
     }
-    private void GetInput()
+
+    // Apply motor torque to the front wheels based on vertical input (acceleration/reverse)
+    private void HandleMotor()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        isBraking = Input.GetKey(KeyCode.Space);
+        float motorTorque = engineForce * _verticalInput;
+        frontLeftWheelCollider.motorTorque = motorTorque;
+        frontRightWheelCollider.motorTorque = motorTorque;
     }
 
-    private void HandleEngine()
-    {
-        frontLeftWheelCollider.motorTorque = engineForce * verticalInput;
-        frontRightWheelCollider.motorTorque = engineForce * verticalInput;
-        currentBrakeForce = isBraking ? brakeForce : 0f;
-    }
-
-    private void ApplyBraking()
-    {
-        frontLeftWheelCollider.brakeTorque = currentBrakeForce;
-        frontRightWheelCollider.brakeTorque = currentBrakeForce;
-        rearLeftWheelCollider.brakeTorque = currentBrakeForce;
-        rearRightWheelCollider.brakeTorque = currentBrakeForce;
-    }
-
+    // Apply steering to the front wheels based on horizontal input (turning)
     private void HandleSteering()
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        _currentSteerAngle = maxSteerAngle * _horizontalInput;
+        frontLeftWheelCollider.steerAngle = _currentSteerAngle;
+        frontRightWheelCollider.steerAngle = _currentSteerAngle;
     }
 
-    private void MoveWheels()
+    // Apply braking force when the spacebar is pressed
+    private void HandleBraking()
     {
-        UpdateWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateWheel(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateWheel(rearLeftWheelCollider, rearLeftWheelTransform);
-        UpdateWheel(rearRightWheelCollider, rearRightWheelTransform);
+        _currentBrakeForce = _isBraking ? brakeForce : 0f;
+        ApplyBrakingToWheels(_currentBrakeForce);
     }
 
-    private void UpdateWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    // Apply brake force to all wheels
+    private void ApplyBrakingToWheels(float brakeForce)
+    {
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
+    }
+
+    // Update the position and rotation of each wheel to match the WheelCollider
+    private void UpdateWheels()
+    {
+        UpdateWheelPose(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheelPose(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheelPose(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheelPose(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    // Update the wheel's transform to match the WheelCollider's pose
+    private void UpdateWheelPose(WheelCollider wheelCollider, Transform wheelTransform)
     {
         Vector3 position;
         Quaternion rotation;
@@ -95,10 +115,11 @@ public class CarController : MonoBehaviour
         wheelTransform.rotation = rotation;
     }
 
+    // Limit the car's speed during braking to avoid sliding
     private void LimitSpeedDuringBraking()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
-        if (isBraking && rigidbody.velocity.magnitude > 20f)
+        if (_isBraking && rigidbody.velocity.magnitude > 20f)
         {
             rigidbody.velocity = rigidbody.velocity.normalized * 20f;
         }
