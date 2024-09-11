ce3188403dd1b008a66acb1ef31df5d8d67116de
Gokul Surendran <105282583+am-Limitless@users.noreply.github.com>
2024-09-11 15:49:23 +0530

    light baked

---
+++ Assets/Script/Game Manager/LevelLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingPannel;
+
+    public Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+
+        StartCoroutine(LoadSceneAsyn(sceneIndex));
+
+    }
+
+    IEnumerator LoadSceneAsyn(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingPannel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
