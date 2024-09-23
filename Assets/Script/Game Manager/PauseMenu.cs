f6d946144a390f37eb82353f63e834bc0adf24a6
Gokul Surendran <105282583+am-Limitless@users.noreply.github.com>
2024-09-23 22:30:51 +0530

    pause menu and audio ui added

--- Assets/Script/Game Manager/PauseMenu.cs
+++ Assets/Script/Game Manager/PauseMenu.cs
@@ -1,12 +1,22 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
-
     private bool isPaused = false;
-
     public GameManager gameManager;
+
+    //Loading level
+    public GameObject loadingPannel;
+    public Slider slider;
+
+    //Music Pannel
+    public GameObject AudioControls;
+
+    public GameObject GameUI;
 
     private void Update()
     {
@@ -43,6 +53,51 @@ public class PauseMenu : MonoBehaviour
         isPaused = false;
 
         gameManager.pauseActive = false;
+    }
+
+    public void RestartLevel()
+    {
+        GameUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadSceneAsyn(currentScene));
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        StartCoroutine(LoadSceneAsyn(0));
+        Time.timeScale = 1f;
+    }
+
+    public void MusicControls()
+    {
+        AudioControls.SetActive(true);
+        pauseMenuUI.SetActive(false);
+
+    }
+
+    public void BackToPause()
+    {
+        AudioControls.SetActive(false);
+        pauseMenuUI.SetActive(true);
+
+    }
+
+    IEnumerator LoadSceneAsyn(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingPannel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
     }
 }
 
